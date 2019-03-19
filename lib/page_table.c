8759489be5a8dd196d2ec49ab7981c219251b1a2
Luke O'Kane <lukecjokane@gmail.com>
2019-03-19 18:19:55 +0000

    Page Table struct

---
+++ lib/page_table.c
@@ -0,0 +1,2 @@
+#include <stdlib.h> 
+#include "page_table.h"
---
+++ lib/page_table.h
@@ -0,0 +1,12 @@
+#ifndef PAGE_TABLE_H 
+#define PAGE_TABLE_H
+#include "memory.h"
+
+// data type for defining page table 
+typedef struct PageTable { 
+ 
+	Memory memory;
+
+} PageTable; 
+ 
+#endif
