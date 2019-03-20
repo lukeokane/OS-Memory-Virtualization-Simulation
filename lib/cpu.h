9875e6adff4354a31ff58ffb87f2e4351fbea27a
Luke O'Kane <lukecjokane@gmail.com>
2019-03-20 12:17:00 +0000

    CPU struct

---
+++ lib/cpu.h
@@ -0,0 +1,13 @@
+#ifndef CPU_H 
+#define CPU_H
+#include "memory_management_unit.h"
+
+// data type for defining CPU
+typedef struct CPU { 
+ 
+	// Member variables
+	MemoryManagementUnit mmu;
+
+} CPU;
+ 
+#endif
